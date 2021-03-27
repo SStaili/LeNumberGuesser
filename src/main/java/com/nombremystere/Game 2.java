66c2472543ae8966b1e00ede93795a64869e0c82
ChloeMrk <chloe.merck@ynov.com>
2021-03-27 16:30:30 +0100

    Create Game 2.java
    Co-Authored-By: SStaili <57040458+SStaili@users.noreply.github.com>

---
+++ src/main/java/com/nombremystere/Game 2.java
@@ -0,0 +1,38 @@
+package com.nombremystere;
+
+import java.util.Scanner;
+
+public class Game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int choice = 0;
+		Nombre n = new Nombre();
+		n.Generer();
+		
+		do {
+			System.out.println("1. Deviner un nombre");
+			System.out.println("2. Challenge IA");
+			System.out.println("3. Quitter l'application");
+			
+			
+			Scanner sc = new Scanner(System.in);
+			choice = sc.nextInt();
+			
+			switch(choice) {
+			case 1:
+				n.Comparer();
+				break;
+			case 2:
+				
+				break;
+			
+			}
+			
+		}while(choice != 3);
+		
+		System.out.println("Fin du programme");
+	}
+
+	
+}
